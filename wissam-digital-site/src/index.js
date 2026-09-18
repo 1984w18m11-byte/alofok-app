5ebd7e12f2f8d80337aadd7cd9663323a1da8bcd
1984w18m11-byte <1984w18m11@gmail.com>
2026-09-18 16:21:59 +0300

    site: update Al-Ufuq Trial download to 1.0.9

--- wissam-digital-site/src/index.js
+++ wissam-digital-site/src/index.js
@@ -2,7 +2,7 @@ const DOWNLOADS = {
   '/download/alofok-trial': {
     app: 'alofok',
     edition: 'trial',
-    url: 'https://github.com/1984w18m11-byte/alofok-app/releases/download/v1.0.7/alofok-trial-1.0.7.apk'
+    url: 'https://github.com/1984w18m11-byte/alofok-app/releases/download/v1.0.9/alofok-trial-1.0.9.apk'
   },
   '/download/wissam-admin': {
     app: 'wissam-digital-admin',
